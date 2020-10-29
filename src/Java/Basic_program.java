8e26aaaca0c1427b66e888f599ffc91726b46f02
Akshay <akshay.gujarathi07@gmail.com>
2020-10-29 11:03:37 +0530

    my first commit

---
+++ src/Java/Basic_program.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Basic_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("This is java program");
+	
+	}
+
+}
